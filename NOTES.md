88f4806cfa9e0636635a6a3b01df2de095af9709
OueslatiGhaith <ghaith.oueslati@enis.tn>
2025-09-24 20:44:10 +0100

    feat: camera move with mouse system

--- NOTES.md
+++ NOTES.md
@@ -1,1 +1,14 @@
+# notes
+
+## games
+
+- [unhaunter](https://github.com/deavid/unhaunter)
+- [digital extinction](https://github.com/DigitalExtinction/Game)
+
+## tutorials
+
 - [camera system](https://www.youtube.com/watch?v=rnqF6S7PfFA)
+
+## resources
+
+- [bevy in prod](https://github.com/Vrixyz/bevy_awesome_prod)
--- crates/input/src/lib.rs
+++ crates/input/src/lib.rs
@@ -1,15 +1,16 @@
 use bevy::{
     input::{ButtonState, keyboard::KeyboardInput},
     prelude::*,
+    window::PrimaryWindow,
 };
 use swarm_camera::camera::CameraMoveEvent;
 
 pub fn plugin(app: &mut App) {
-    app.add_systems(Update, move_camera_arrow_system);
+    app.add_systems(Update, (move_camera_arrow_system, move_camera_mouse_system));
 }
 
 // TODO: lerp
-pub fn move_camera_arrow_system(
+fn move_camera_arrow_system(
     mut key_events: EventReader<KeyboardInput>,
     mut move_events: EventWriter<CameraMoveEvent>,
 ) {
@@ -35,4 +36,45 @@ pub fn move_camera_arrow_system(
 
         move_events.write(CameraMoveEvent(direction));
     }
+}
+
+fn move_camera_mouse_system(
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut was_moving: Local<bool>,
+    mut move_events: EventWriter<CameraMoveEvent>,
+) {
+    const MOVE_MARGIN: f32 = 2.0;
+
+    let Ok(window) = window_query.single() else {
+        return;
+    };
+
+    let Some(cursor) = window.cursor_position() else {
+        if *was_moving {
+            *was_moving = false;
+            move_events.write(CameraMoveEvent(Vec2::ZERO));
+        }
+
+        return;
+    };
+
+    let mut movement = Vec2::ZERO;
+    if cursor.x < MOVE_MARGIN {
+        movement.x += 1.0;
+    } else if cursor.x > (window.width() - MOVE_MARGIN) {
+        movement.x -= 1.0;
+    }
+
+    if cursor.y < MOVE_MARGIN {
+        movement.y += 1.0;
+    } else if cursor.y > (window.height() - MOVE_MARGIN) {
+        movement.y -= 1.0;
+    }
+
+    if (movement != Vec2::ZERO) == *was_moving {
+        return;
+    }
+    *was_moving = movement != Vec2::ZERO;
+
+    move_events.write(CameraMoveEvent(movement));
 }
